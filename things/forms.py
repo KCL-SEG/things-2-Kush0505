494d0d0de10e8d89ae59614d8306b94019ce98da
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-12-09 16:18:12 +0000

    finish thing_forms

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,30 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from things.models import Thing
+
+class ThingForm(forms.Form):
+
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120, widget=forms.Textarea(attrs={'name':'body', 'style': 'height: 3em'}))
+    quantity = forms.IntegerField(
+        validators=[MinLengthValidator(0), MaxLengthValidator(50)]
+    )
+
+    class Meta:
+        fields = (
+            "name",
+            "description",
+            "quantity",
+        )
+
+    def save(self, commint=True):
+
+        returnedUser = thing.objects.create_user(
+            name = self.cleaned_data.get('name'),
+            description = self.cleaned_data.get('description'),
+            quantity = self.cleaned_data.get('quantity')
+        )
+
+        return returnedUser
